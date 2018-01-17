af0ca429c3e29a1e20f2459ff9648536d43db869
chenqian <1096011884@qq.com>
2018-01-17 14:18:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenq/mygittext/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.chenq.mygittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+
+2018年1月17日14:15:51
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
